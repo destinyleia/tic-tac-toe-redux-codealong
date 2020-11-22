75ae15e96e33a9dcb676fe3b156fcf82abd72431
destinyleia <destinylindgrenc@gmail.com>
2020-11-22 14:59:18 +0100

    tictac

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { game } from 'reducers/game'
 import { Player } from 'components/Player'
 
 export const Square = ({ value, index }) => {
+  const dispatch = useDispatch()
+  // useDispatch allows us to trigger things inside our store. We dispatch from the folder "reducers" in game.js
+
   const handleClick = () => {
-    // TODO send the captureSquare action
+    dispatch(game.actions.captureSquare({ index }))
   }
 
   return (
